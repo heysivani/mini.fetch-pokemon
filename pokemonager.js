474f6afece9ed28e1c62aff3e06e717f8be6cff9
heysivani <heysivani@gmail.com>
2020-10-09 14:48:03 +0900

    findUnderWeight returns correct array but isn't passing test???
    Co-authored-by: kenny01123 <kenny01123@users.noreply.github.com>
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>

--- pokemonager.js
+++ pokemonager.js
@@ -1,36 +1,28 @@
 (() => {
   class Pokemonager {
-    // This should return an array of all the names of n Pokemon from the Pokemon API.
-    // findNames(n) {
-    //   const names=[];
-    //   // Your code here.
-    //   fetch(`https://pokeapi.co/api/v2/pokemon/?offset=20&limit=${n}`)
-    //   .then(res => res.json())
-    //   .then(json => json.results)
-    //   .then(arr =>  Promise.all(arr.forEach(element=>names.push(element.push))))
-
-    // }
-
     findNames(n) {
       // Your code here.
       //https://pokeapi.co/api/v2/pokemon/
-      fetch(`https://pokeapi.co/api/v2/pokemon/?offset=20&limit=${n}`)
+      return fetch(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${n}`)
       .then(res => res.json())
       .then(json => json.results)
-      .then(arr =>  {
-        return arr.map(element => element.name)
-        //return arr.forEach(element=>names.push(element.push));
-      })
+      .then(arr => arr.map(x => x.name))
     }
 
     // This should return an array of all the Pokemon that are under a particular weight.
 
     findUnderWeight(weight) {
-      // Your code here.
-      // ** LIMIT TO THE FIRST 10 POKEMON
-      // We don't want to make too many unnecessary calls to the Pokemon API
+      let list = [];
+
+      for(let i = 1; i <= 10; i++) {
+        list.push(
+          fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+          .then(res => res.json())
+          )
+      }
+      return Promise.all(list)
+        .then(arr => arr.filter(pokemon => pokemon.weight < weight).map(pokemon => pokemon.name))
     }
   }
-
   window.Pokemonager = Pokemonager;
 })();
