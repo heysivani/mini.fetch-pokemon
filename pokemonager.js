7366706e83386a13def3dae7ad44802d04447caf
heysivani <heysivani@gmail.com>
2020-10-09 12:33:45 +0900

    wrote basic structure for findNames
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>
    Co-authored-by: kenny01123 <kenny01123@users.noreply.github.com>

--- pokemonager.js
+++ pokemonager.js
@@ -1,8 +1,26 @@
 (() => {
   class Pokemonager {
     // This should return an array of all the names of n Pokemon from the Pokemon API.
+    // findNames(n) {
+    //   const names=[];
+    //   // Your code here.
+    //   fetch(`https://pokeapi.co/api/v2/pokemon/?offset=20&limit=${n}`)
+    //   .then(res => res.json())
+    //   .then(json => json.results)
+    //   .then(arr =>  Promise.all(arr.forEach(element=>names.push(element.push))))
+
+    // }
+
     findNames(n) {
       // Your code here.
+      //https://pokeapi.co/api/v2/pokemon/
+      fetch(`https://pokeapi.co/api/v2/pokemon/?offset=20&limit=${n}`)
+      .then(res => res.json())
+      .then(json => json.results)
+      .then(arr =>  {
+        return arr.map(element => element.name)
+        //return arr.forEach(element=>names.push(element.push));
+      })
     }
 
     // This should return an array of all the Pokemon that are under a particular weight.
